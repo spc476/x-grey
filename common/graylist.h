caabeaecef898fed0ff50e44577a9a9731740a97
Sean Conner <spc@lucy.(none)>
2007-08-22 02:47:54 -0400

    Common definitions

--- common/graylist.h
+++ common/graylist.h
@@ -10,10 +10,11 @@
 
 typedef unsigned char  byte;
 typedef unsigned long  IP;
+typedef unsigned long  flags;
 typedef unsigned short Port;
 typedef unsigned short unet16;
 
-enum 
+enum
 {
   MTA_SENDMAIL,
   MTA_QMAIL,
@@ -28,6 +29,22 @@ enum
   CMD_GRAYLIST,
   CMD_GRAYLIST_RESP,
   CMD_MAX
+};
+
+enum
+{
+  ERR_OKAY,
+  ERR_VERSION_NOT_SUPPORTED,
+  ERR_MTA_NOT_SUPPORTED,
+  ERR_TYPE_NOT_SUPPORTED,
+  ERR_BAD_DATA,
+  ERR_MAX
+};
+
+enum
+{
+  GRAYLIST_NAY,
+  GRAYLIST_YEA
 };
 
 typedef struct graylist_response
@@ -49,16 +66,22 @@ typedef struct graylist_request
   byte   data[1];
 };
 
+#define F_WHITELIST	(1uL << 0)
+#define F_GRAYLIST	(1uL << 1)
+#define F_TRUNCFROM	(1uL << 2)
+#define F_TRUNCTO	(1uL << 3)
+#define F_IPv6		(1uL << 4)
+
 typedef struct tuple
 {
-  time_t ctime;
-  time_t atime;
-  size_t fsize;
-  size_t tsize;
-  int    white;
-  byte   ip  [16];
-  char   from[114];
-  char   to  [114];
+  time_t       ctime;
+  time_t       atime;
+  size_t       fromsize;
+  size_t       tosize;
+  flags        f;
+  byte         ip  [16];
+  char         from[114];
+  char         to  [114];
 } *Tuple;
 
 /***************************************************************/
