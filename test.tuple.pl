647afcfd5465496e6b40a64494d838e6e459f67c
Sean Conner <spc@lucy.(none)>
2007-09-14 16:57:20 -0400

    Added some test files.

---
+++ test.tuple.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+use strict;
+
+my $line;
+my @tuple;
+
+while($line = <STDIN>)
+{
+  chomp $line;
+  @tuple = split / /,$line;
+  print <<EOF;
+request=smtpd_access_policy
+sender=$tuple[1]
+recipient=$tuple[2]
+client_address=$tuple[0]
+
+EOF
+}
