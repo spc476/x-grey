832feb67603b79274232416aac55e28a8a3b02a0
Sean Conner <spc@lucy.(none)>
2007-10-30 16:30:35 -0400

    New testing program.

---
+++ test/src/mktuples.c
@@ -0,0 +1,206 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <getopt.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+#include <cgilib/memory.h>
+#include <cgilib/ddt.h>
+#include <cgilib/stream.h>
+#include <cgilib/util.h>
+
+#include "../../common/src/graylist.h"
+#include "../../common/src/crc32.h"
+
+#define min(a,b)	((a) < (b)) ? (a) : (b)
+
+/***********************************************************************/
+
+enum
+{
+  OPT_INPUT_FILE = OPT_USER,
+  OPT_OUTPUT_FILE,
+  OPT_COUNT
+};
+
+/************************************************************************/
+
+char   *g_inputfile  = NULL;
+char   *g_outputfile = "precanned-tuple.%02d";
+char   *g_secret     = "decafbad";
+size_t  g_secretsize = 8;
+size_t  g_count      = 65536uL;
+
+static const struct option mc_options[] =
+{
+  { "input-file"	, required_argument	, NULL	, OPT_INPUT_FILE  } ,
+  { "output-file"	, required_argument	, NULL	, OPT_OUTPUT_FILE } ,
+  { "count"		, required_argument	, NULL	, OPT_COUNT	  } ,
+  { "secret"		, required_argument	, NULL	, OPT_SECRET	  } ,
+  { "help"		, no_argument		, NULL	, OPT_HELP	  } ,
+  { NULL		, 0			, NULL	, 0		  }
+};
+
+/**********************************************************************/
+
+void	parse_command_line	(int,char *[]);
+
+/**********************************************************************/
+
+int main(int argc,char *argv[])
+{
+  Stream                   input;
+  int                      out       = -1;
+  int                      filecount = 1;
+  size_t                   count;
+  byte                     outpacket[1500];
+  struct graylist_request *glq;
+  
+  MemInit();
+  DdtInit();
+  StreamInit();
+
+  input = StdinStream;
+  
+  parse_command_line(argc,argv);
+  
+  if (g_inputfile)
+    input = FileStreamRead(g_inputfile);
+  
+  count = g_count;
+  
+  while(!StreamEOF(input))
+  {
+    char   *line;
+    char   *from;
+    char   *to;
+    size_t  sfrom;
+    size_t  sto;
+    size_t  packetsize;
+    byte   *p;
+    CRC32   crc;
+
+    if (count == g_count)
+    {
+      char filename[BUFSIZ];
+      
+      close(out);
+      sprintf(filename,g_outputfile,filecount++);
+      out = open(filename,O_WRONLY | O_CREAT | O_TRUNC,0644);
+      if (out == -1)
+      {
+        perror(filename);
+        exit(EXIT_FAILURE);
+      }
+      count = 0;
+    }
+      
+    line = LineSRead(input);
+    if (empty_string(line)) { MemFree(line); continue; }
+    
+    glq = (struct graylist_request *)outpacket;
+    p   = glq->data;
+    
+    glq->version = htons(VERSION);
+    glq->MTA     = htons(MTA_POSTFIX);
+    glq->type    = htons(CMD_GRAYLIST);
+    glq->ipsize  = htons(4);
+    
+    *p++ = strtoul(line,&from,10); from++;
+    *p++ = strtoul(from,&from,10); from++;
+    *p++ = strtoul(from,&from,10); from++;
+    *p++ = strtoul(from,&from,10); from++;
+    
+    to = strchr(from,' ');
+    if (to == NULL) continue;
+    *to++ = '\0';
+    
+    if (strcmp(from,"-") == 0)
+      from = "";
+    
+    if (strcmp(to,"-") == 0)
+      to = "";
+    
+    sfrom = min(strlen(from),200);
+    sto   = min(strlen(to),200);
+    
+    glq->fromsize = htons(sfrom);
+    glq->tosize   = htons(sto);
+    
+    memcpy(p,from,sfrom); p += sfrom;
+    memcpy(p,to,  sto);   p += sto;
+    
+    packetsize = (size_t)(p - outpacket);
+    crc        = crc32(INIT_CRC32,&glq->version,packetsize - sizeof(CRC32));
+    crc        = crc32(crc,g_secret,g_secretsize);
+    glq->crc   = htonl(crc);
+    
+    write(out,&packetsize,sizeof(packetsize));
+    write(out,outpacket,packetsize);
+    MemFree(line);
+    count++;
+  }
+  
+  close(out);
+  StreamFree(input);
+  return(EXIT_SUCCESS);
+}
+
+/**********************************************************************/
+
+void parse_command_line(int argc,char *argv[])
+{
+  while(1)
+  {
+    int option = 0;
+    
+    switch(getopt_long_only(argc,argv,"",mc_options,&option))
+    {
+      case EOF:
+           return;
+      case OPT_NONE:
+           break;
+      case OPT_INPUT_FILE:
+           g_inputfile = dup_string(optarg);
+           break;
+      case OPT_OUTPUT_FILE:
+           g_outputfile = dup_string(optarg);
+           break;
+      case OPT_COUNT:
+           g_count = strtoul(optarg,NULL,10);
+           break;
+      case OPT_SECRET:
+           g_secret = dup_string(optarg);
+           g_secretsize = strlen(g_secret);
+           break;
+      case OPT_HELP:
+      default:
+           LineSFormat(
+           	StderrStream,
+           	"$ $ $ L",
+           	"usage: %a [options]\n",
+           	"\t--input-file <file>	(stdin)\n"
+           	"\t--output-file <file>	(%b)\n"
+           	"\t--secret <string>	(%c)\n"
+           	"\t--count <num>	(%d)\n"
+           	"\t--help		(this text)\n"
+           	"\n",
+           	argv[0],
+           	g_outputfile,
+           	g_secret,
+           	(unsigned long)g_count
+           );
+           exit(EXIT_FAILURE);
+    }
+  }
+}
+
+/***********************************************************************/
+
---
+++ test/src/sendtuples.c
@@ -0,0 +1,285 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <getopt.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <signal.h>
+
+#include <cgilib/memory.h>
+#include <cgilib/ddt.h>
+#include <cgilib/stream.h>
+#include <cgilib/util.h>
+
+#include "../../common/src/graylist.h"
+#include "../../common/src/crc32.h"
+#include "../../common/src/util.h"
+
+#define min(a,b)	((a) < (b)) ? (a) : (b)
+
+/***********************************************************************/
+
+enum
+{
+  OPT_INPUT_FILE = OPT_USER
+};
+
+/************************************************************************/
+
+	/*----------------------
+	; required for linkage
+	;-----------------------*/
+	
+char   *c_host         = DEF_LHOST;
+int     c_port         = 0;
+char   *c_timeformat   = "%c";
+int     c_log_facility = 0;
+int     c_log_level    = 0;
+char   *c_log_id       = NULL;
+int     cf_debug       = 0;
+void  (*cv_report)(int,char *,char *,...) = report_syslog;
+
+	/*--------------------------
+	; our global variables
+	;--------------------------*/
+
+struct sockaddr_in  g_raddr;
+socklen_t           g_raddrsize  = sizeof(struct sockaddr_in);
+char               *g_rhost      = DEF_RHOST;
+int                 g_rport      = DEF_RPORT;
+char               *g_inputfile  = "precanned-tuple.01";
+
+static const struct option mc_options[] =
+{
+  { "server"		, required_argument	, NULL	, OPT_RHOST	  } ,
+  { "server-port"	, required_argument	, NULL	, OPT_RPORT	  } ,
+  { "host"		, required_argument	, NULL	, OPT_HOST	  } ,
+  { "port"		, required_argument	, NULL	, OPT_PORT	  } ,
+  { "input-file"	, required_argument	, NULL	, OPT_INPUT_FILE  } ,
+  { "help"		, no_argument		, NULL	, OPT_HELP	  } ,
+  { NULL		, 0			, NULL	, 0		  }
+};
+
+/**********************************************************************/
+
+void	parse_command_line	(int,char *[]);
+
+/**********************************************************************/
+
+int main(int argc,char *argv[])
+{
+  struct stat     status;
+  struct hostent *remote;
+  int             fh;
+  void           *vba;
+  size_t         *pps;
+  char           *packet;
+  int		  sock;
+  pid_t           child;
+  int             rc;
+  
+  MemInit();
+  DdtInit();
+  StreamInit();
+  
+  parse_command_line(argc,argv);
+  
+  /*-----------------------------
+  ; cache server address:port
+  ;----------------------------*/
+  
+  remote = gethostbyname(g_rhost);
+  if (remote == NULL)
+  {
+    perror(g_rhost);
+    return(EXIT_FAILURE);
+  }
+  
+  memcpy(&g_raddr.sin_addr.s_addr,remote->h_addr,remote->h_length);
+  g_raddr.sin_family = AF_INET;
+  g_raddr.sin_port   = htons(g_rport);
+  
+  /*--------------------------------------------------
+  ; open the precanned packets, and map into memory.
+  ;---------------------------------------------------*/
+  
+  fh = open(g_inputfile,O_RDONLY);
+  if (fh == -1)
+  {
+    perror(g_inputfile);
+    return(EXIT_FAILURE);
+  }
+  
+  rc = fstat(fh,&status);
+  if (rc == -1)
+  {
+    perror("stat()");
+    close(fh);
+    return(EXIT_FAILURE);
+  }
+  
+  vba = mmap(NULL,status.st_size, PROT_READ,MAP_PRIVATE,fh,0);
+  if (vba == (void *)-1)
+  {
+    perror("mmap()");
+    close(fh);
+    return(EXIT_FAILURE);
+  }
+  
+  packet = vba;
+  sock   = create_socket(c_host,c_port,SOCK_DGRAM);
+
+#if 0
+  child = fork();
+  if (child == (pid_t)-1)
+  {
+    perror("fork()");
+    close(sock);
+    munmap(vba,status.st_size);
+    close(fh);
+    return(EXIT_FAILURE);
+  }
+#else
+  child = 0;
+#endif
+
+  if (child == 0)	/* parent */
+  {
+    size_t  size  = status.st_size;
+    size_t  count = 0;
+    ssize_t rrc;
+    
+    while(size)
+    {
+      pps = (size_t *)packet;
+      packet += sizeof(size_t);
+      rrc = sendto(
+    		sock,
+    		packet,
+    		*pps,
+    		0,
+    		(const struct sockaddr *)&g_raddr,
+    		g_raddrsize
+    	);
+      packet += *pps;
+      size -= (sizeof(size_t) + *pps);
+      count++;
+
+#if 0
+      {
+        byte inpacket[1500];
+        struct sockaddr_in sip;
+        socklen_t          sipsize;
+       
+        sipsize = sizeof(struct sockaddr_in);
+        rrc     = recvfrom(
+       			sock,
+       			inpacket,
+       			sizeof(inpacket),
+       			0,
+       			(struct sockaddr *)&sip,
+       			&sipsize
+       		);
+      }
+#endif
+
+    }
+    
+    LineSFormat(StdoutStream,"L","%a packets sent\n",(unsigned long)count);
+  }
+  else			/* child */
+  {
+    byte               inpacket[1500];
+    struct sockaddr_in sip;
+    socklen_t          sipsize;
+    ssize_t            rrc;
+    
+    while(1)
+    {
+      sipsize = sizeof(struct sockaddr_in);
+      rrc     = recvfrom(
+      			sock,
+      			inpacket,
+      			sizeof(inpacket),
+      			0,
+      			(struct sockaddr *)&sip,
+      			&sipsize
+      		);
+    }
+    _exit(EXIT_SUCCESS);
+  }  
+
+  sleep(1);
+#if 0
+  kill(child,SIGTERM);
+#endif
+
+  close(sock);
+  munmap(vba,status.st_size);
+  close(fh);
+  return(EXIT_SUCCESS);
+}
+
+/**********************************************************************/
+
+void parse_command_line(int argc,char *argv[])
+{
+  while(1)
+  {
+    int option = 0;
+    
+    switch(getopt_long_only(argc,argv,"",mc_options,&option))
+    {
+      case EOF:
+           return;
+      case OPT_NONE:
+           break;
+      case OPT_RHOST:
+           g_rhost = dup_string(optarg);
+           break;
+      case OPT_RPORT:
+           g_rport = strtoul(optarg,NULL,10);
+           break;
+      case OPT_HOST:
+           c_host = dup_string(optarg);
+           break;
+      case OPT_PORT:
+           c_port = strtoul(optarg,NULL,10);
+           break;
+      case OPT_INPUT_FILE:
+           g_inputfile = dup_string(optarg);
+           break;
+      case OPT_HELP:
+      default:
+           LineSFormat(
+           	StderrStream,
+           	"$ $ $ i $ i",
+           	"usage: %a [options]\n"
+           	"\t--input-file <file>\t(%b)\n"
+           	"\t--server <host>\t\t(%c)\n"
+           	"\t--server-port <num>\t(%d)\n"
+           	"\t--host <host>\t\t(%e)\n"
+           	"\t--port <num>\t\t(%f)\n"
+           	"\t--help\n"
+           	"\n",
+           	argv[0],
+           	g_inputfile,
+           	g_rhost,
+           	g_rport,
+           	c_host,
+           	c_port
+           );
+           exit(EXIT_FAILURE);
+    }
+  }
+}
+
+/***********************************************************************/
+
