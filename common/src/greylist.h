2eb17b9b7f6f866c90e118aee5a5f15f4a2e8bae
Sean Conner <spc@lucy.roswell.conman.org>
2009-03-26 22:31:12 -0400

    V:1.0.9 starts ... now!

--- common/src/greylist.h
+++ common/src/greylist.h
@@ -32,7 +32,7 @@
 	; this way for hysterical reasons.
 	;------------------------------------------------*/
 
-#define PROG_VERSION	"1.0.8"
+#define PROG_VERSION	"1.0.9"
 #define VERSION		0x0100
 
 /********************************************************************/
