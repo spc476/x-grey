93cd1edd002087e56677da1b0731e97e2e57323f
spc <spc@conman.org>
2010-05-10 19:07:23 -0400

    Updated Copyright year on all files.  Also make sure Licensing verbiage was correct

--- control/src/main.c
+++ control/src/main.c
@@ -1,7 +1,6 @@
 /***************************************************************************
 *
-* Copyright 2007 by Sean Conner.
-*
+* Copyright 2010 by Sean Conner.
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
